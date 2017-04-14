5d763d46b9f791f3b2289c0d2752be7dc46a4f97
Dmitrii Makarkin <Dmitrii_Makarkin@epam.com>
2017-04-14 13:54:34 +0300

    updated package json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.port || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -34,4 +34,4 @@ app.get('/about', function(req, res) {
     });
 });
 
-app.listen(3000);
+app.listen(port);
